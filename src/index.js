e6e5e2c6f7c8b64038e51cdb1a4ccc697bab7cbc
svdfsdev <58512937+svdfsdev@users.noreply.github.com>
2020-03-02 02:46:58 +0300

    feat: added implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function toReadable (number) {
-  
+    
+    let arr0till9 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let arr10till19 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let arrDecades = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    let wordNumber = "";
+
+    if (number > 99) {
+        wordNumber = arr0till9[ Math.floor( number / 100) ] + ' hundred ';
+    }
+
+    if ( (number % 100) > 9 && (number % 100) < 20 ) {
+        return wordNumber = wordNumber + arr10till19[ Math.floor( number % 10 ) ];
+    } else if ( Math.floor(number % 100 / 10) != 0) {
+        wordNumber = wordNumber + arrDecades[ Math.floor(number % 100 / 10) - 2 ] + ' ';
+    }
+
+    if (number % 10 > 0 && number > 20) {
+        wordNumber = wordNumber + arr0till9[number % 10];
+    } else if (number < 10) {
+        wordNumber = arr0till9[number];
+    }
+
+    return wordNumber.trim();
 }
