3cf51ec0fd228cfc581278289dee2221a8f6c912
svdfsdev <58512937+svdfsdev@users.noreply.github.com>
2020-09-15 16:26:51 +0300

    refactor: refactor code

--- src/index.js
+++ src/index.js
@@ -1,26 +1,49 @@
-module.exports = function toReadable (number) {
-    
-    let arr0till9 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    let arr10till19 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    let arrDecades = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+module.exports = function toReadable(number) {
+    let units = {
+        0: "zero",
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+    };
 
-    let wordNumber = "";
+    let secondDozen = {
+        10: "ten",
+        11: "eleven",
+        12: "twelve",
+        13: "thirteen",
+        14: "fourteen",
+        15: "fifteen",
+        16: "sixteen",
+        17: "seventeen",
+        18: "eighteen",
+        19: "nineteen",
+    };
 
-    if (number > 99) {
-        wordNumber = arr0till9[ Math.floor( number / 100) ] + ' hundred ';
-    }
+    let dozens = {
+        2: "twenty",
+        3: "thirty",
+        4: "forty",
+        5: "fifty",
+        6: "sixty",
+        7: "seventy",
+        8: "eighty",
+        9: "ninety",
+    };
 
-    if ( (number % 100) > 9 && (number % 100) < 20 ) {
-        return wordNumber = wordNumber + arr10till19[ Math.floor( number % 10 ) ];
-    } else if ( Math.floor(number % 100 / 10) != 0) {
-        wordNumber = wordNumber + arrDecades[ Math.floor(number % 100 / 10) - 2 ] + ' ';
-    }
+    let wordNumber = '';
+    let countHundred = parseInt(number / 100);
+    let countDozen = parseInt((number % 100 / 10));
+    let countUnit = parseInt(number % 10);
 
-    if (number % 10 > 0 && number > 20) {
-        wordNumber = wordNumber + arr0till9[number % 10];
-    } else if (number < 10) {
-        wordNumber = arr0till9[number];
-    }
+    wordNumber += countHundred ? units[countHundred] + ' hundred' : '';
+    wordNumber += countDozen ? ' ' + (dozens[countDozen] || secondDozen[number % 100]) : '';
+    wordNumber += (countDozen !== 1 && number % 10 !== 0) || number < 9 ? ' ' + units[countUnit] : '';
 
     return wordNumber.trim();
 }
